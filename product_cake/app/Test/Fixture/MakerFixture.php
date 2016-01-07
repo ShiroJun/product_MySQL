954d71e22eb5cdc514c69a56635ccd6cb97cf088
PracticeMan <maruq7777@gmail.com>
2016-01-07 18:31:05 +0900

    MCV

---
+++ product_cake/app/Test/Fixture/MakerFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * MakerFixture
+ *
+ */
+class MakerFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'maker_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'created' => '2016-01-07 10:21:32',
+			'modified' => '2016-01-07 10:21:32',
+			'maker_name' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+
+}
---
+++ product_cake/app/Test/Fixture/ProductFixture.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * ProductFixture
+ *
+ */
+class ProductFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'product_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'maker_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'created' => '2016-01-07 10:24:20',
+			'modified' => '2016-01-07 10:24:20',
+			'product_name' => 'Lorem ipsum dolor sit amet',
+			'maker_id' => 1
+		),
+	);
+
+}
