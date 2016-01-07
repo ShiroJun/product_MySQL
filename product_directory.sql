1334a6fa930eee6e0ddf6048887993bb561c88d3
ShiroJun <0909robinson@gimail.com>
2016-01-07 18:01:16 +0900

    バージョン２

--- product_directory.sql
+++ product_directory.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2016 年 1 朁E07 日 09:35
+-- Generation Time: 2016 年 1 朁E07 日 10:00
 -- サーバのバージョン： 5.6.26
 -- PHP Version: 5.6.12
 
@@ -19,6 +19,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `product_directory`
 --
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `makers`
+--
+
+CREATE TABLE IF NOT EXISTS `makers` (
+  `id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `maker_name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -39,6 +52,12 @@ CREATE TABLE IF NOT EXISTS `products` (
 --
 
 --
+-- Indexes for table `makers`
+--
+ALTER TABLE `makers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -48,6 +67,11 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `makers`
+--
+ALTER TABLE `makers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `products`
 --
